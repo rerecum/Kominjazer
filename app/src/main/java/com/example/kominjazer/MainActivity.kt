d45a132d7c8ec07deecd238e0e049eb7915fa48f
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-11-30 09:12:33 +0100

    zmiana designu apliakcji, jeden spinner jako pierwsze miasto a reszta to checkboxy

--- app/src/main/java/com/example/kominjazer/MainActivity.kt
+++ app/src/main/java/com/example/kominjazer/MainActivity.kt
@@ -20,15 +20,11 @@ class MainActivity : AppCompatActivity() {
         )
 
         val City = resources.getStringArray(R.array.City)
-
         val adapter = ArrayAdapter(
             this,
             android.R.layout.simple_spinner_item, City
         )
         spinnery[0].adapter = adapter
-
-        if (adapter.getItem(0) == adapter.getItem(1))
-            Toast.makeText(this@MainActivity,"działa",Toast.LENGTH_SHORT)
 
         spinnery[0].onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
